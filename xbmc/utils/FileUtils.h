319b355abb656eab90ce39d1d28c10dd3923ddcb
adieu <adieu@adieu.cn>
2011-03-31 16:01:55 +0800

    added function to caculate file hash for shooter.cn

--- xbmc/utils/FileUtils.h
+++ xbmc/utils/FileUtils.h
@@ -7,4 +7,5 @@ public:
   static bool DeleteItem(const CFileItemPtr &item);
   static bool RenameFile(const CStdString &strFile);
   static bool SubtitleFileSizeAndHash(const CStdString &path, CStdString &size, CStdString &hash);
+  static bool SubtitleFileSizeAndHash2(const CStdString &path, CStdString &digest1, CStdString &digest2, CStdString &digest3, CStdString &digest4);
 };
